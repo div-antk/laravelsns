8cda7abf8f2da62161568aeeb64e7255ca946eb0
div-antk <51283468+owaii@users.noreply.github.com>
2020-03-25 20:10:24 +0900

    add article_post_system

--- app/Article.php
+++ app/Article.php
@@ -5,9 +5,13 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-
 class Article extends Model
 {
+    protected $fillable = [
+        'title',
+        'body',
+    ];
+
     public function user(): BelongsTo
     {
         return $this->belongsTo('App\User');
