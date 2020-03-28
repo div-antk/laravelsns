09977e79b9b764470899bb387cc8e8205dc154bc
div-antk <51283468+owaii@users.noreply.github.com>
2020-03-28 19:15:53 +0900

    fix controller

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -6,7 +6,7 @@
 
     @guest
     <li class="nav-item">
-      <a class="nav-link" href="">ユーザー登録</a>
+      <a class="nav-link" href="{{ route('register') }}">ユーザー登録</a>
     </li>
     @endguest
 
