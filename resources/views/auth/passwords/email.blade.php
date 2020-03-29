7d4d2db6c81c4a35920180c33cffd2e4046c7fad
div-antk <51283468+owaii@users.noreply.github.com>
2020-03-29 17:07:55 +0900

    fix email_blade

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -8,29 +8,31 @@
       <div class="mx-auto col col-12 col-sm-11 col-md-9 col-lg-7 col-xl-6">
         <h1 class="text-center"><a class="text-dark" href="/">memo</a><h1>
         <div class="card mt-3">
-          <h2 class="h3 card-title text-center mt-2">パスワード再設定</h2>
+          <div class="card-body text-center">
+            <h2 class="h3 card-title text-center mt-2">パスワード再設定</h2>
 
-          @include('error_card_list')
+            @include('error_card_list')
 
-          @if (session('status'))
-            <div class="dard-text alert alert-success">
-              {{ session('status') }}
-            </div>
-          @endif
-
-          <div class="card-text">
-            <form method="POST" action="{{ route('password.email') }}">
-              @csrf
-
-              <div class="md-form">
-                <label for="email">メールアドレス</label>
-                <input class="form-controll" type="text" id="email" name="email" required>
+            @if (session('status'))
+              <div class="dard-text alert alert-success">
+                {{ session('status') }}
               </div>
+            @endif
 
-              <button class="btn btn-block blue-gradient mt-2 mb-2" type="submit">メール送信</button>
+            <div class="card-text">
+              <form method="POST" action="{{ route('password.email') }}">
+                @csrf
+
+                <div class="md-form">
+                  <label for="email">メールアドレス</label>
+                  <input class="form-control" type="text" id="email" name="email" required>
+                </div>
+
+                <button class="btn btn-block blue-gradient mt-2 mb-2" type="submit">メール送信</button>
+              
+              </form>
             
-            </form>
-          
+            </div>
           </div>
         </div>
       </div>
