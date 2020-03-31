23b441130c1fa26581adcb5117e5fe41170efc4d
div-antk <51283468+owaii@users.noreply.github.com>
2020-03-31 21:51:00 +0900

    add js & like_view

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
-require('./bootstrap');
+// Laravelの全画面で共通的に使用することを想定したJS
+
+import './bootstrap'
+import Vue from 'vue'
+import ArticleLike from './components/ArticleLike'
+
+const app = new Vue({
+  el: '#app',
+  components: {
+    ArticleLike,
+  }
+})
