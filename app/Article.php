8acd4566f8fce4fb735628abf66e6894c61d7d70
div-antk <51283468+owaii@users.noreply.github.com>
2020-04-01 23:40:07 +0900

    add isLikedBy_method

--- app/Article.php
+++ app/Article.php
@@ -4,6 +4,9 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+// 記事モデルとユーザーモデルの関係は多対多
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
 
 class Article extends Model
 {
@@ -15,5 +18,21 @@ class Article extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo('App\User');
+    }
+
+    public function likes(): BelomgsToMany
+    {
+        // 第一引数には関連するモデルの名前、第二引数には中間テーブル名
+        // likesテーブルにはcreated_at、update_atがあるのでwithTimestampsを付ける
+        return $this->belingsToMany('App\User', 'likes')->withTimestamps();
+    }
+
+    public function isLikedBy(?User $user): bool
+    {
+        return $user
+            // nullableで引数がnullであることも許容する
+            // 三項演算子で$userがnullかどうかで処理を分ける
+            // nullであればfalseを返す
+            ? (bool)$this->likes->where('id', $user->id)->count()
     }
 }
