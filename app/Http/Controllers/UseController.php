2ae6a36654a1f45e0ac786f9b1fdf35409172443
div-antk <51283468+owaii@users.noreply.github.com>
2020-04-05 15:46:47 +0900

    fix user_conttoller

--- app/Http/Controllers/UseController.php
+++ app/Http/Controllers/UseController.php
@@ -1,7 +1,5 @@
 <?php
 
-// ユーザーページの表示やフォローのアクションメソッドを持たせる
-
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,10 +1,22 @@
 <?php
 
+// ユーザーページの表示やフォローのアクションメソッドを持たせる
+
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function show(string $name)
+    {
+        // ルーティングに定義したURL /users/{name} の部分が渡る
+        $user = User::where('name', $name)->first();
+
+        // ユーザーページのBladeを表示し、ユーザーモデルの入った変数 $user を渡す
+        return view('users.show', [
+            'user' => $user,
+        ]);
+    }
 }
